d85d2e29aef7e0c6c603dbc98f3b0ab06da2cc1c
NgocThanh <nnthanh9779@gmail.com>
2019-03-19 20:32:06 +0700

    add notification service

--- ShopOnline.Web/app/components/product_categories/productCategoryListController.js
+++ ShopOnline.Web/app/components/product_categories/productCategoryListController.js
@@ -1,15 +1,16 @@
 ﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope', 'apiService'];
+    //khởi tạo tự động các đối tượng service
+    productCategoryListController.$inject = ['$scope', 'apiService', 'notificationService'];
 
-    function productCategoryListController($scope, apiService) {
+    function productCategoryListController($scope, apiService, notificationService) {
 
         $scope.productCategories = [];
         $scope.page = 0;
         $scope.pagesCount = 0;
         $scope.getProductCategories = getProductCategories;
-        $scope.keyword = '';
+        $scope.keyword = ''; //keyword searching
 
         $scope.search = search;
 
@@ -22,10 +23,16 @@
                 params: {
                     keyword: $scope.keyword,
                     page: page,
-                    pageSize: 20
+                    pageSize: 2
                 }
             };
             apiService.get('/api/productcategory/getall', config, function (result) {
+                if (result.data.TotalCount == 0) {
+                    notificationService.displayWarning("không có bảng ghi nào được tìm thấy.");
+                }
+                else {
+                    notificationService.displaySuccess("Đã tìm thấy " + result.data.TotalCount + " bản ghi.");
+                }
                 $scope.productCategories = result.data.Items;
                 $scope.page = result.data.Page;
                 $scope.pagesCount = result.data.TotalPages;
---
+++ ShopOnline.Web/app/shared/service/notificationService.js
@@ -0,0 +1,40 @@
+﻿(function (app) {
+    app.factory('notificationService', notificationService);
+
+    function notificationService() {
+        toastr.options = {
+            "debug": false,
+            "positionClass": "toast-top-right",
+            "onclick": null,
+            "fadeIn": 300, //show 0.3s
+            "fadeOut": 1000, //hide 1s
+            "timeOut": 3000, //show time 3s
+            "extendedTimeOut": 1000
+        };
+
+        return {
+            displaySuccess: displaySuccess,
+            displayError: displayError,
+            displayWarning: displayWarning,
+            displayInfo: displayInfo
+        };
+
+        function displaySuccess(message) {
+            toastr.success(message);
+        }
+        function displayWarning(message) {
+            toastr.warning(message);
+        }
+        function displayError(error) {
+            if (Array.isArray(error)) {
+                error.forEach(function (err) {
+                    toastr.error(err);
+                });
+            }
+            toastr.error(error);
+        }
+        function displayInfo(message) {
+            toastr.info(message);
+        }
+    }
+})(angular.module('shoponline.common'));
